3c260295c5bbbff90a0b891fdda090eeced8a830
a-collado <alex.collado.gar@gmail.com>
2024-07-11 23:52:42 +0200

    test inicial giroscopio

--- scripts/player/gyro_manager.gd
+++ scripts/player/gyro_manager.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@export var test: Node3D
+@export_range(0, 2) var sensibility: float = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	print(Input.get_gravity())
+	var gyro_rotation = Input.get_gravity()
+	test.rotation.y = -gyro_rotation.x * sensibility
