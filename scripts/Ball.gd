5272e7ab915dcb848d799c773cc20827d420a5db
a-collado <alex.collado@proton.me>
2024-07-31 15:33:56 +0200

    Arreglado desync en multiplayer

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -5,11 +5,22 @@ class_name Ball
 @export var future_position_node: Node3D
 @export_range(0, 0.3, 0.01) var future_position_time: float = 0.1
 
+var frames_between_hits: int = 10
+@export var last_hit: int = 0
+
 @rpc("any_peer", "call_local", "reliable")
 func kick(force: Vector3):
-	var velocity = linear_velocity / 4
-	linear_velocity = Vector3(0,0,0)
-	apply_impulse(force - velocity)
+
+	print("Force: %s" % force)
+	if force != Vector3.ZERO and last_hit > frames_between_hits:
+		var velocity = linear_velocity / 4
+		linear_velocity = Vector3(0,0,0)
+		print("Velocity: %s" % velocity)
+		print("Linear velocity: %s" % linear_velocity)
+		apply_impulse(force - velocity)
+		print("Resulting force:  %s" % (force - velocity))
+		last_hit = 0
+		print("HIT")
 
 @rpc("any_peer", "call_local", "reliable")
 func reset(spawn: Vector3):
@@ -21,3 +32,6 @@ func reset(spawn: Vector3):
 func _process(_delta):
 	var future_position = transform.origin + linear_velocity * future_position_time
 	future_position_node.transform.origin = future_position
+
+func _physics_process(_delta: float) -> void:
+	last_hit += 1
