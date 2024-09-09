adc0d21a78972d29258e0c99addb28e070e1e6b0
a-collado <alex.collado@proton.me>
2024-09-09 13:44:30 +0200

    Solucionado error musica menu

--- scripts/global/menu_music.gd
+++ scripts/global/menu_music.gd
@@ -3,6 +3,8 @@ extends AudioStreamPlayer
 @onready var menu_music : AudioStreamOggVorbis = preload("res://assets/Audio/Musica/Groove Dungeon.ogg")
 @onready var game_music : AudioStreamOggVorbis = preload("res://assets/Audio/Musica/ElectroFest.ogg")
 
+var in_menu: bool = true
+
 func _ready() -> void:
 	stream = menu_music
 	volume_db = -10
@@ -18,6 +20,7 @@ func _game_entered() -> void:
 		await get_tree().process_frame
 
 	if(get_node_or_null("/root/Game")):
+		in_menu = false
 		stop()
 		stream = game_music
 		play()
@@ -29,6 +32,10 @@ func _game_exited() -> void:
 	if(get_node_or_null("/root/StartScreen")):
 		if not $"/root/StartScreen".is_connected("tree_exited", _game_entered):
 			$"/root/StartScreen".connect("tree_exited", _game_entered)
+
+	if in_menu:
+		return
+	in_menu = true
 	stop()
 	stream = menu_music
 	play()
